118a42b53352d0b88c6c2a76687d0320d52f0eab
SobolevaLiza <91316745+SobolevaLiza@users.noreply.github.com>
2021-10-07 20:08:40 +0300

    Add files via upload

--- 1.4..py
+++ 1.4..py
@@ -1,8 +1,9 @@
 # Переменная seconds хранит в себе некоторое количество секунд. Переведите это число в дни:часы:минуты:секунды.
+# -- coding: utf-8--
 seconds = int(input('Введите количество секунд:'))
-if seconds > 60:
-    print((seconds // 60), 'минут')
-if seconds > 3600:
-    print((seconds // 3600), 'часов')
-if seconds > 86400:
-    print((seconds // 86400), 'дней')
+
+dnei = seconds // 86400
+chasov = (seconds % 86400) // 3600
+minut = ((seconds % 86400) % 3600) // 60
+sec = ((seconds % 86400) % 3600) % 60
+print(dnei, 'суток', chasov, 'часов', minut, 'минут', sec, 'секунд')
